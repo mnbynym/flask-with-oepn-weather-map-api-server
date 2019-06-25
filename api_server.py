926a68c48c12d340d8ee87de9090bb7a875cfeb8
mnbynym <29582847+mnbynym@users.noreply.github.com>
2019-06-25 14:50:42 +0900

    Create api_server.py

---
+++ api_server.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
+
+APP_ID = "2f15e17c4acb2fb77b1e3e6ae76e5981"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&appid={app_id}"
+
+@app.route('/weather/<string:city>', methods=['GET'])
+def get_all(city):
+	request_url = BASE_URL.format(city=city, app_id=APP_ID)
+	result = requests.get(request_url)
+	return jsonify(json.loads(result.text)), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
